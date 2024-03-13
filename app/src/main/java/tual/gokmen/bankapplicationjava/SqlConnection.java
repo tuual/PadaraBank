f354ab5b9a88ac4c36c22813341917db67802a7f
tuual <tual.gokmen41@gmail.com>
2024-03-13 16:58:41 +0300

    2030234

--- app/src/main/java/tual/gokmen/bankapplicationjava/SqlConnection.java
+++ app/src/main/java/tual/gokmen/bankapplicationjava/SqlConnection.java
@@ -3,9 +3,12 @@ package tual.gokmen.bankapplicationjava;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
 import android.os.StrictMode;
 import android.util.Log;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -16,78 +19,95 @@ import java.sql.Struct;
 
 import javax.xml.transform.Result;
 
-    public class SqlConnection extends SQLiteOpenHelper {
-        Connection connection;
-        String username, password, ip, port, database;
-        private Statement st;
+public class SqlConnection extends SQLiteOpenHelper {
+    Connection connection;
+    String username, password, ip, port, database;
+    private Statement st;
 
-        public SqlConnection(Context context) {
-            super(context, "OnMuhasebe.db", null, 2);
-            username = "biltekbilisim";
-            database = "OnMuhasebe";
-            password = "Bilisim20037816";
-            port = "1433";
-            ip = "213.254.137.231";
+    public SqlConnection(Context context) {
+        super(context, "OnMuhasebe.db", null, 2);
+        username = "biltekbilisim";
+        database = "OnMuhasebe";
+        password = "Bilisim20037816";
+        port = "1433";
+        ip = "213.254.137.231";
 
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
-            String connectionUrl = null;
+        String connectionUrl = null;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            connectionUrl = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";" + "databasename=" + database + ";user=" + username + ";password=" + password + ";";
+            connection = DriverManager.getConnection(connectionUrl);
+            st = connection.createStatement();
 
-            try {
-                Class.forName("net.sourceforge.jtds.jdbc.Driver");
-                connectionUrl = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";" + "databasename=" + database + ";user=" + username + ";password=" + password + ";";
-                connection = DriverManager.getConnection(connectionUrl);
-                Toast.makeText(context, "Bağlandı", Toast.LENGTH_SHORT).show();
-            } catch (Exception e) {
-                Log.i("VeriTabanıHatası", e.getMessage());
-            }
-        }
+        } catch (SQLException e) {
+            Log.i("VeriTabanıHatası", e.getMessage());
 
-        public void execSQL(String query, Context cont) throws SQLException {
-            try {
-                st = connection.createStatement();
-                st.executeUpdate(query);
-                Toast.makeText(cont, "bağlandı", Toast.LENGTH_SHORT).show();
-            } catch (SQLException ex) {
-                throw new SQLException(ex);
-            }
-        }
-
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            db.execSQL("CREATE TABLE musteri(musteriID INTEGER PRIMARY KEY AUTOINCREMENT, musteriTC nvarchar(15), musteriParola nvarchar(10), musteriBakiye INTEGER);");
-        }
-
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("DROP TABLE IF EXISTS musteri");
-            onCreate(db);
-        }
-
-        public boolean checkUser(String tcno, String parola) throws SQLException {
-            try{
-                st = connection.createStatement();
-                String query = "Select * from musteri WHERE musteriTC='"+tcno+"' AND musteriParola='"+ parola + "'";
-                ResultSet resultSet = st.executeQuery(query);
-                return resultSet.next();
-            }catch (SQLException e){
-                e.printStackTrace();
-                return false;
-            }
-        }
-        public ResultSet getUserInfo(String tcno,String parola) throws SQLException {
-            try {
-                String query = "SELECT * FROM musteri WHERE musteriTC= '"+tcno+"'AND musteriParola='"+parola+ "'";
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(query);
-                return resultSet;
-            } catch (SQLException e) {
-                e.printStackTrace();
-                throw e;
-            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
+
+    public void execSQL(String query, Context cont) throws SQLException {
+        try {
+            st = connection.createStatement();
+            st.executeUpdate(query);
+        } catch (SQLException ex) {
+            throw new SQLException(ex);
+        }
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE musteri(musteriID INTEGER PRIMARY KEY AUTOINCREMENT, musteriTC nvarchar(15), musteriParola nvarchar(10), musteriBakiye INTEGER);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS musteri");
+        onCreate(db);
+    }
+
+    public boolean checkUser(String tcno, String parola) throws SQLException {
+        try {
+            st = connection.createStatement();
+            String query = "Select * from musteri WHERE musteriTC='" + tcno + "' AND musteriParola='" + parola + "'";
+            ResultSet resultSet = st.executeQuery(query);
+            return resultSet.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public ResultSet getUserInfo(String tcno, String parola) throws SQLException {
+        try {
+            String query = "SELECT * FROM musteri WHERE musteriTC= '" + tcno + "'AND musteriParola='" + parola + "'";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            return resultSet;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public String getCustomerBalanceByID(String customerID) {
+        try {
+            String query = "SELECT musteriBakiye FROM musteri WHERE musteriID='" + customerID + "'";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            if (resultSet.next()) {
+                return resultSet.getString("musteriBakiye");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
 
 
 
--- app/src/main/java/tual/gokmen/bankapplicationjava/frmLogin.java
+++ app/src/main/java/tual/gokmen/bankapplicationjava/frmLogin.java
@@ -1,6 +1,7 @@
 package tual.gokmen.bankapplicationjava;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.text.Editable;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.checkbox.MaterialCheckBox;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.android.material.textview.MaterialTextView;
@@ -33,44 +35,78 @@ public class frmLogin extends AppCompatActivity {
     private Button btn;
     private MaterialTextView textView;
     boolean deger = true;
-    private String tcno,parola;
+    private String tcno, parola;
     private SqlConnection connection;
-    public  String musteriID,musteriTC,musteriParola,musteriBakiye;
+    public String musteriID, musteriTC, musteriParola, musteriBakiye;
+    private MaterialCheckBox checkBox;
+    private String kaydedilecekTC, kaydedilecekSifre;
+    private SharedPreferences sharedPreferences;
+    private Boolean checkboxValue;
+    private SharedPreferences.Editor editor;
+    private boolean veriizni;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frm_login);
         setID();
+
         txtsifre.setVisibility(View.GONE);
         txtlayoutsifre.setVisibility(View.GONE);
         btn.setVisibility(View.GONE);
-        connection = new SqlConnection(getApplicationContext());
 
+        setupSharedPreferences();
+
+
+        if (!kaydedilecekTC.isEmpty()) {
+            veriizni = true;
+        }
+        //
+        if (veriizni) {
+            loadsaveData();
+        }
+
+
+        //
+
+        connection = new SqlConnection(getApplicationContext());
         clickFunc();
         getTime();
 
     }
-    private void setID(){
-        txttc =(TextInputEditText) findViewById(R.id.txtTcNo);
-        txtsifre = (TextInputEditText)findViewById(R.id.txtSifre);
+
+    private void setID() {
+        txttc = (TextInputEditText) findViewById(R.id.txtTcNo);
+        txtsifre = (TextInputEditText) findViewById(R.id.txtSifre);
         txtlayoutsifre = (TextInputLayout) findViewById(R.id.textinputlayout2);
         btn = (Button) findViewById(R.id.btnSign);
         textView = (MaterialTextView) findViewById(R.id.materialTextView);
+
+
     }
-    private void clickFunc(){
+
+    private void clickFunc() {
+
+
         txttc.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
                 txtsifre.setVisibility(View.GONE);
                 txtlayoutsifre.setVisibility(View.GONE);
+
+
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (txttc.length() ==11){
+
+                if (txttc.length() == 11) {
                     txtsifre.setVisibility(View.VISIBLE);
                     txtlayoutsifre.setVisibility(View.VISIBLE);
                 }
+
+
             }
 
             @Override
@@ -85,7 +121,8 @@ public class frmLogin extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (txtsifre.length() == 6){
+
+                if (txtsifre.length() == 6) {
                     btn.setVisibility(View.VISIBLE);
                 }
             }
@@ -96,70 +133,88 @@ public class frmLogin extends AppCompatActivity {
             }
         });
 
-        btn.setOnClickListener(view -> {
-            String tcNo = txttc.getText().toString();
-            String parola = txtsifre.getText().toString();
+        btn.setOnClickListener(view -> loginuser(txttc.getText().toString(),txtsifre.getText().toString()));
 
-            try {
-                if (connection.checkUser(tcNo, parola)) {
-                    Toast.makeText(getApplicationContext(), "Giriş başarılı", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(getApplicationContext(), frmAnaMenu.class);
-                    checkUserFunc(tcNo,parola);
-                    startActivity(intent);
-                    intent.putExtra("musteriID",musteriID);
-                    intent.putExtra("musteriTC",musteriTC);
-                    intent.putExtra("musteriParola",musteriParola);
-                    intent.putExtra("musteriBakiye",musteriBakiye);
-                    startActivity(intent);
-                    frmLogin.this.overridePendingTransition(
-                            R.anim.animate_card_enter,
-                            R.anim.animate_card_exit
-                    );
-
-                } else {
-                    Toast.makeText(getApplicationContext(), "Giriş başarısız", Toast.LENGTH_SHORT).show();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        });
     }
-    private void getTime(){
+
+    private void loginuser(String tcNo,String parola) {
+
+
+
+
+
+        try {
+            if (connection.checkUser(tcNo, parola)) {
+                Toast.makeText(getApplicationContext(), "Giriş başarılı", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), frmAnaMenu.class);
+                editor.putString("tc", txttc.getText().toString());
+                editor.apply();
+                checkUserFunc(tcNo, parola);
+                intent.putExtra("musteriID", musteriID);
+                intent.putExtra("musteriTC", musteriTC);
+                intent.putExtra("musteriParola", musteriParola);
+                intent.putExtra("musteriBakiye", musteriBakiye);
+                startActivity(intent);
+                overridePendingTransition(R.anim.animate_card_enter, R.anim.animate_card_exit);
+            } else {
+                Toast.makeText(getApplicationContext(), "Giriş başarısız", Toast.LENGTH_SHORT).show();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getTime() {
 
         Calendar cal = Calendar.getInstance();
         int timeofDay = cal.get(Calendar.HOUR_OF_DAY);
-        if (timeofDay >= 0 && timeofDay <12) {
+        if (timeofDay >= 0 && timeofDay < 12) {
             textView.setText(R.string.text1);
-        } else if (timeofDay >= 12 && timeofDay <17) {
+        } else if (timeofDay >= 12 && timeofDay < 17) {
             textView.setText(R.string.text3);
 
         } else if (timeofDay >= 17 && timeofDay < 20) {
             textView.setText(R.string.text4);
-        }
-        else if (timeofDay >= 20 && timeofDay < 24){
+        } else if (timeofDay >= 20 && timeofDay < 24) {
             textView.setText(R.string.text2);
         }
 
 
     }
-    private void checkUserFunc(String tc,String pass)  {
-        try {
-            ResultSet resultSet = connection.getUserInfo(tc,pass);
 
-            if (resultSet.next()){
+    public void checkUserFunc(String tc, String pass) {
+        try {
+            ResultSet resultSet = connection.getUserInfo(tc, pass);
+
+            if (resultSet.next()) {
                 musteriID = resultSet.getString("musteriID");
                 musteriTC = resultSet.getString("musteriTC");
                 musteriParola = resultSet.getString("musteriParola");
                 musteriBakiye = resultSet.getString("musteriBakiye");
-                String userInfo = "Müşteri ID: " + musteriID + "\nMüşteri TC: " + musteriTC + "\nMüşteri Parola: " + musteriParola + "\nMüşteri Bakiye: " + musteriBakiye;
-                Toast.makeText(this, userInfo, Toast.LENGTH_SHORT).show();
+
 
             }
+            editor.putString("hesapBakiye",musteriBakiye);
+            editor.putString("hesapID",musteriID);
+            editor.apply();
         } catch (SQLException e) {
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
 
     }
 
+    private void loadsaveData() {
+        txttc.setText(kaydedilecekTC);
+
+        btn.setVisibility(View.VISIBLE);
+        txtsifre.setVisibility(View.VISIBLE);
+        txtlayoutsifre.setVisibility(View.VISIBLE);
+    }
+
+    private void setupSharedPreferences() {
+        sharedPreferences = getSharedPreferences("skipPage", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        kaydedilecekTC = sharedPreferences.getString("tc", "");
+    }
 
 }
